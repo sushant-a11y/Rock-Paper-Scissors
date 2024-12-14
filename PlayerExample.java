28176da5fa818b68803961d0e0b4b1790cc56e7f
Sushant Indupuru <si6854@pleasantonusd.net>
2024-12-14 11:40:44 -0800

    add TwoIndupuruSushant.java and start strategy

--- PlayerExample.java
+++ PlayerExample.java
@@ -1,10 +1,10 @@
 import java.util.*;
 /**
  * Each player or team will create their own Player, this is only an example....
- * 
+ * <p>
  * Your code MUST INCLUDE a move method with the following signature. To insure that, you must implement
  * the Player interface
- * 
+ * <p>
  * You can create additional methods but must have the move method
  * 
  */
@@ -15,10 +15,10 @@ public class PlayerExample implements Player
      * An example of a method - replace this comment with your own
      * You must create some kind of logic of what to play against your opponent...start thinking!
      * 
-     * @return      the move you want to play against opponent
-     *              "r" - rock
-     *              "p" - paper
-     *              "s" - scissors
+     * @return      the move you want to play against opponent<br>
+     *              "r" - rock<br>
+     *              "p" - paper<br>
+     *              "s" - scissors<br>
      *              anything else - forfeit the turn
      */
     public String move(String [] myMoves, String [] opponentMoves, int myScore, int opponentScore)
---
+++ TwoIndupuruSushant.java
@@ -0,0 +1,29 @@
+public class TwoIndupuruSushant implements Player{
+    final String name="TwoIndupuruSushant";
+    final String strategy = "Markov chain statistical model";
+    /**
+     * This strategy uses the markov chain statistical model to calculate the most probable opponent move
+     *
+     * @return      the move played against opponent<br>
+     *              "r" - rock<br>
+     *              "p" - paper<br>
+     *              "s" - scissors<br>
+     *              anything else - forfeit the turn
+     */
+    @Override
+    public String move(String[] myMoves, String[] opponentMoves, int myScore, int opponentScore) {
+        return "";
+    }
+    /**
+     * Returns the name of the player
+     *
+     * @return      the name of the player
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
+    public String getStrategy() {
+        return strategy;
+    }
+}
